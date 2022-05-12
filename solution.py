ded0e0c4f7189dc3e07c522d73d1c9093e7d37bf
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2022-05-11 23:52:37 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -117,6 +117,7 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 +
                     bytes])[0]
                     #Fill in start
+                    print (" %d   rtt=%.0f ms %s" % (ttl,(timeReceived -t)*1000, addr[0]))
                     #You should add your responses to your lists here
                     #Fill in end
                 elif types == 3:
@@ -124,19 +125,20 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
                     #You should add your responses to your lists here 
-                    tracelist2.append(bytes)
+                    print (" %d   rtt=%.0f ms %s" % (ttl,(timeReceived -t)*1000, addr[0]))
                     #Fill in end
                 elif types == 0:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
                     #You should add your responses to your lists here and return your list if your destination IP is met
-                    tracelist2.append(bytes)
+                    print(" %d rtt=%.Of ms %s" % (ttl,(timeReceived-timeSent)*1000, addr[0]))
+                    return
                     #Fill in end
                 else:
                     #Fill in start
                     #If there is an exception/error to your if statements, you should append that to your list here
-                    tracelist2.append(herror)
+                    print("error")
                     #Fill in end
                 break
             finally:
